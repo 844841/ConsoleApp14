081bdb7f83052bcbd0f1aa1b540ef3c58a975487
15341a1205 <41323544+15341a1205@users.noreply.github.com>
2020-02-20 14:54:39 +0530

    Initial commit

---
+++ strings.cs
@@ -0,0 +1,3 @@
+﻿1>------ Build started: Project: ConsoleApp15, Configuration: Debug Any CPU ------
+1>  ConsoleApp15 -> d:\Users\Admin\source\repos\ConsoleApp15\ConsoleApp15\bin\Debug\ConsoleApp15.exe
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
